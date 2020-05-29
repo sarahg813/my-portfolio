97ea03d141f7da4ce240480de74f9c2ec4292ab4
sarahg813 <goon.sarah@gmail.com>
2020-05-29 03:20:48 -0400

    new portfolio

---
+++ sarahg.me/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer role="footer">
+      <div className="footer-root">&copy; 2020 Sarah Goon</div>
+    </footer>
+  );
+};
+
+export default Footer;
